2a4baf941c177d7c6ab4c39ca485688e70bfd279
benshabbat <benshabbat27@gmail.com>
2024-01-31 21:18:43 +0200

    after check start

--- helloworld/obj/Debug/net8.0/helloworld.AssemblyInfo.cs
+++ helloworld/obj/Debug/net8.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f34871c8fc4381a631dd8301e2d393612380e75e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09e7329b466a79bd509420ce12bbcbd741195f17")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
